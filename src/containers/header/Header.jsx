31b948dfa4fa43f842f7b8375bee6863b6b0fbc2
Jason <jason@s-164-67-226-237.resnet.ucla.edu>
2023-05-16 14:25:21 -0700

    Fix image resizing

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
-          Let's build something amazing with GPT-3 OpenAI
+          Let&apos;s Build Something amazing with GPT-3 OpenAI
         </h1>
         <p>
           Yet bed any for travelling assistance indulgence unpleasing. Not
@@ -16,17 +16,20 @@ const Header = () => {
           alteration boisterous the attachment. Party we years to order allow
           asked of.
         </p>
+
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email Address" />
           <button type="button">Get Started</button>
         </div>
+
         <div className="gpt3__header-content__people">
-          <img src={people} alt="people" />
+          <img src={people} />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
+
       <div className="gpt3__header-image">
-        <img src={ai} alt="ai" />
+        <img src={ai} />
       </div>
     </div>
   );
